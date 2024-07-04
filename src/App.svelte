fb2b822aa2c7beff70d4d53f8e5e6c6685d69f9e
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-04 20:35:14 +0800

    removed sveltestrap stylesheet as unocss alr provides it

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,6 @@
   export let title = "En Jie's Portfolio";
 </script>
 
-<svelte:head>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-</svelte:head>
-
 
 <head><title>{title}</title></head>
 

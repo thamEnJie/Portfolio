b0a2e2efed7d271e752046bbd6129dd1bd46f1ba
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-06 11:58:21 +0800

    fixed spacing for intro

--- src/lib/home/Intro.svelte
+++ src/lib/home/Intro.svelte
@@ -1,17 +1,18 @@
 <script>
     const src = "assets/ultiphoto.png"
+
+    const txt = "text-center lg:text-left"
 </script>
 
 <div class="min-h-screen flex justify-center items-center pb-25">
-    <div class="flex flex-row">
-        <img {src} class="row-span-1 rounded-10 h-45 my-auto ml-auto mr-2.5" alt="Tham En Jie" />
-        <div class="row-span-1 my-auto mr-auto ml-2.5">
-            <p>
-            <h1>Hi! I'm En Jie :)</h1>
+    <div class="flex flex-col lg:flex-row gap-5">
+        <img {src} class="row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
+        <div class="row-span-1">
+            <h1 class="{txt}">Hi! I'm En Jie :)</h1>
             <p>
             <strong>Deep Learner • Enthusiastic Maker • Ecological Lover</strong>
             </p>
-            <p class="text-wrap">
+            <p class="text-wrap {txt}">
             I value <u>deep learning</u> for more <b>accurate</b> and <b>complete</b> understanding.<br>
             I enjoy <u>designing</u> and <u>making</u> products with <b>CAD Modelling</b> and <b>3D Printing</b>.<br>
             I aim to create <u>solutions</u> and <u>findings</u> that <b>productively</b> impacts ecology.
@@ -19,9 +20,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    h1, p {
-        text-align: left;
-    }
-</style>

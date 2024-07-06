0797066395c40d2e754218453b643d138ac46609
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-06 10:11:42 +0800

    updated intro

--- src/lib/home/Intro.svelte
+++ src/lib/home/Intro.svelte
@@ -1,35 +1,27 @@
 <script>
-    import { Col, Image, Row } from "@sveltestrap/sveltestrap";
     const src = "assets/ultiphoto.png"
 </script>
 
-<div>
-    <Row>
-        <Col xs="12" sm="4">
-            <Image {src} alt="Image of Tham En Jie" fluid />
-        </Col>
-        <Col xs="12" sm="8">
+<div class="min-h-screen flex justify-center items-center pb-25">
+    <div class="flex flex-row">
+        <img {src} class="row-span-1 rounded-10 h-45 my-auto ml-auto mr-2.5" alt="Image of Tham En Jie" />
+        <div class="row-span-1 my-auto mr-auto ml-2.5">
             <p>
             <h1>Hi! I'm En Jie :)</h1>
             <p>
             <strong>Deep Learner • Enthusiastic Maker • Ecological Lover</strong>
             </p>
-            <p>
+            <p class="text-wrap">
             I value <u>deep learning</u> for more <b>accurate</b> and <b>complete</b> understanding.<br>
             I enjoy <u>designing</u> and <u>making</u> products with <b>CAD Modelling</b> and <b>3D Printing</b>.<br>
             I aim to create <u>solutions</u> and <u>findings</u> that <b>productively</b> impacts ecology.
             </p>
-        </Col>
-    </Row>
+        </div>
+    </div>
 </div>
 
 <style>
     h1, p {
         text-align: left;
     }
-    /* div {
-        position: relative;
-        height: 100vh;
-        top:25vh;
-    } */
 </style>
--- src/lib/home/home.svelte
+++ src/lib/home/home.svelte
@@ -1,22 +1,15 @@
 <script lang="ts">
-  import { Col, Container } from "@sveltestrap/sveltestrap"
-
   import Intro from "./Intro.svelte";
   import Interests from "./Interests.svelte";
   import HardSkills from "./HardSkills.svelte";
   import SoftSkills from "./SoftSkills.svelte";
   import Showcase from "./Showcase.svelte";
 
-  const sections = [Interests, HardSkills, SoftSkills, Showcase]
+  const sections = [Intro, Interests, HardSkills, Showcase]
 </script>
 
-<Container>
-  <Col>
-    <Intro />
-    {#each sections as sect}
-      <div class="mb-5">
-        <svelte:component this={sect} />
-      </div>
-    {/each}
-  </Col>
-</Container>
+{#each sections as sect}
+  <div class="mb-5">
+    <svelte:component this={sect} />
+  </div>
+{/each}

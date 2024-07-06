71220011fad7c057d03f369c5f28c3bf91a4013a
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-06 12:20:53 +0800

    added arrow to intro

--- src/lib/home/Intro.svelte
+++ src/lib/home/Intro.svelte
@@ -1,22 +1,23 @@
 <script>
     const src = "assets/ultiphoto.png"
-
-    const txt = "text-center lg:text-left"
 </script>
 
-<div class="min-h-screen flex justify-center items-center pb-25">
-    <div class="flex flex-col lg:flex-row gap-5">
-        <img {src} class="row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
-        <div class="row-span-1">
-            <h1 class="{txt}">Hi! I'm En Jie :)</h1>
-            <p>
-            <strong>Deep Learner • Enthusiastic Maker • Ecological Lover</strong>
-            </p>
-            <p class="text-wrap {txt}">
-            I value <u>deep learning</u> for more <b>accurate</b> and <b>complete</b> understanding.<br>
-            I enjoy <u>designing</u> and <u>making</u> products with <b>CAD Modelling</b> and <b>3D Printing</b>.<br>
-            I aim to create <u>solutions</u> and <u>findings</u> that <b>productively</b> impacts ecology.
-            </p>
+<div class="flex flex-col">
+    <div class="min-h-screen flex justify-center items-center pb-25 z-1">
+        <div class="flex flex-col lg:flex-row gap-5">
+            <img {src} class="row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
+            <div class="row-span-1 lg:text-left">
+                <h1>Hi! I'm En Jie :)</h1>
+                <p>
+                <strong>Deep Learner • Enthusiastic Maker • Ecological Lover</strong>
+                </p>
+                <p class="text-wrap">
+                I value <u>deep learning</u> for more <b>accurate</b> and <b>complete</b> understanding.<br>
+                I enjoy <u>designing</u> and <u>making</u> products with <b>CAD Modelling</b> and <b>3D Printing</b>.<br>
+                I aim to create <u>solutions</u> and <u>findings</u> that <b>productively</b> impacts ecology.
+                </p>
+            </div>
         </div>
     </div>
+    <span class="text-9xl text-slate-200 z-0 -mt-50">↓</span>
 </div>

fff3937a52d4325cddd251443c0eceed807240ca
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-07 10:07:00 +0800

    fixe height in intro

--- src/lib/home/Intro.svelte
+++ src/lib/home/Intro.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="flex flex-col">
-    <div class="min-h-screen flex justify-center items-center pb-25 z-1">
+    <div class="heightMod flex justify-center items-center pb-25 z-1">
         <div class="flex flex-col lg:flex-row gap-5">
-            <img {src} class="row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
+            <img {src} class="object-contatin row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
             <div class="row-span-1 lg:text-left">
                 <h1>Hi! I'm En Jie :)</h1>
                 <p>
@@ -19,5 +19,11 @@
             </div>
         </div>
     </div>
-    <span class="text-9xl text-slate-200 z-0 -mt-50">↓</span>
+    <span class="text-9xl text-slate-100 z-0 -mt-50">↓</span>
 </div>
+
+<style>
+    .heightMod {
+        min-height: 90vh;
+    }
+</style>

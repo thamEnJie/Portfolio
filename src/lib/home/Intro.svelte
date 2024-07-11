c668ad4d7cef32a366884ff857b25b70bf1eb5fa
ThamEnJie <tham_en_jie@s2021.ssts.edu.sg>
2024-07-11 10:52:07 +0800

    update intro styles

--- src/lib/home/Intro.svelte
+++ src/lib/home/Intro.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="flex flex-col">
-    <div class="heightMod flex justify-center items-center pb-25 z-1">
+    <div class="min-h-screen flex justify-center items-center pb-25 z-1">
         <div class="flex flex-col lg:flex-row gap-5">
-            <img {src} class="object-contatin row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
+            <img {src} class="object-contain row-span-1 rounded-10 h-45 mx-auto" alt="Tham En Jie" />
             <div class="row-span-1 lg:text-left">
                 <h1>Hi! I'm En Jie :)</h1>
                 <p>
@@ -19,11 +19,5 @@
             </div>
         </div>
     </div>
-    <span class="text-9xl text-slate-100 z-0 -mt-50">↓</span>
+    <span class="text-9xl text-slate-100 z-0 -mt-40">↓</span>
 </div>
-
-<style>
-    .heightMod {
-        min-height: 90vh;
-    }
-</style>
